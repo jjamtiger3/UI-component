09b10584874d2c9808252c15e41d442d7da679d0
seois830926 <idmahoro@gmail.com>
2022-12-13 21:33:00 +0900

    button css 수정 및 icon구현전 커밋

--- ui-react-component/src/components/Button.tsx
+++ ui-react-component/src/components/Button.tsx
@@ -4,8 +4,13 @@ import buttonStyles from '../styles/components/Button.module.css'
 
 
 const Button: React.FC<IButton> = function (props: any) {
+    // TODO Icon
     return (
-        <button onClick={props.onClick}>{props.label}</button>
+        <div className={buttonStyles['button-container']}>
+            <button onClick={props.onClick}>
+                {props.label}
+            </button>
+        </div>
     )
 }
 
